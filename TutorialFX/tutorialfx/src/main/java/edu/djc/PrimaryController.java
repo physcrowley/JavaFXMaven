9c8bb7c0e5c8812505729c501042c03db1165c26
physcrowley <crowlda@ecolecatholique.ca>
2020-10-20 20:44:12 -0400

    renamed controller to match fxml name

--- TutorialFX/tutorialfx/src/main/java/edu/djc/PrimaryController.java
+++ TutorialFX/tutorialfx/src/main/java/edu/djc/PrimaryController.java
@@ -6,7 +6,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class MainViewController 
+public class PrimaryController 
 {
     @FXML
     Label number;
